3974631afd7dfac297b1fa31f3ecb21372cc95f6
aleconte <aurelieleconte.al@gmail.com>
2021-10-03 16:12:05 +0200

    reste norme

--- philo.c
+++ philo.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 int	ft_strlen(char *s)
 {
 	int i;
@@ -36,6 +38,13 @@ int	ft_atoi(const char *str)
 		i++;
 	}
 	return (nb * sign);
+}
+
+int		ft_free(void **ptr)
+{
+	free(*ptr);
+	*ptr = NULL;
+	return (0);
 }
 
 #include <pthread.h>
@@ -117,21 +126,6 @@ int		print_state(char *state, t_phil *phil, t_info *info)
 	return (0);
 }
 
-int		exit_philo(t_info *info, t_phil **phil, int failure)
-{
-	//printf("EXIT TA MERE\n");
-	//free;
-
-	pthread_mutex_lock(&info->check_off);
-	info->turn_off = 1;
-	pthread_mutex_unlock(&info->check_off);
-	destroy(info, phil);
-	if (failure)
-		exit(1);
-	exit(0);
-	return (0);
-}
-
 long 	get_time(t_info *info)
 {
 	long time_sec;
@@ -140,16 +134,13 @@ long 	get_time(t_info *info)
 	if (gettimeofday(&info->time, NULL))
 		return (0);
 	time_sec = info->time.tv_sec - info->start.tv_sec;
-	//printf("sec => %ld - %ld = %ld\n", info->time.tv_sec, info->start.tv_sec, time_sec);
 	time_msec = info->time.tv_usec / 1000 - info->start.tv_usec / 1000;
-	//printf("msec => %d - %d = %ld\n", info->time.tv_usec / 1000, info->start.tv_usec /1000, time_msec);
 	if (time_msec < 0)
 	{
 		time_msec += 1000;
 		time_sec -= 1;
 	}
 	time_sec = (time_sec * 1000) + time_msec;
-	//printf("GET TIME = %ld\n", time_sec);
 	return (time_sec);
 }
 
@@ -188,10 +179,41 @@ int		take_drop_fork(int take, t_phil *phil)
 	return (0);
 }
 
+void 	eat(t_phil *phil)
+{
+	if (phil->info->nb_philo == 1)
+	{
+		mutex_fork(1, phil, 0);
+		ft_usleep(phil->info->time_die + 10000, phil->info);
+		mutex_fork(0, phil, 0);
+		return ;
+	}
+	take_drop_fork(1, phil);
+	pthread_mutex_lock(&phil->check_meal);
+	phil->last_eat = get_time(phil->info);
+	pthread_mutex_unlock(&phil->check_meal);
+	print_state("=====is eating\n", phil, phil->info);
+	ft_usleep(phil->info->time_eat, phil->info);
+	//if(ft_usleep(phil->info->time_eat /** 1000*/, phil->info))
+	//	printf("%ld %d AWAKE from eat\n", get_time(phil->info), i);//DEBUG
+	phil->count_eat++;
+	if (phil->count_eat == phil->info->nb_eat)
+	{
+		//printf("%ld %d -----has finish\n", get_time(phil->info), i);
+		pthread_mutex_lock(&phil->check_meal);
+		phil->finish = 1;
+		pthread_mutex_unlock(&phil->check_meal);
+		usleep(20);	
+	}
+	take_drop_fork(0, phil);
+}
+
 void	 *routine(void *ptr)
 {
-	t_phil *phil = ptr;
+	t_phil *phil;
 	int i;
+	
+	phil = (t_phil *)ptr;
 	i =  phil->index;
 	if (i % 2 != 0)
 		usleep(100);
@@ -201,53 +223,19 @@ void	 *routine(void *ptr)
 		//if(!check_turn_off(phil->info))
 		print_state("is thinking\n", phil, phil->info);
 		if(!check_turn_off(phil->info))
-		{
-			take_drop_fork(1, phil);
-			phil->last_eat = get_time(phil->info);
-			print_state("=====is eating\n", phil, phil->info);
-			ft_usleep(phil->info->time_eat /** 1000*/, phil->info);
-			//if(ft_usleep(phil->info->time_eat /** 1000*/, phil->info))
-			//	printf("%ld %d AWAKE from eat\n", get_time(phil->info), i);//DEBUG
-			phil->count_eat++;
-			if (phil->count_eat == phil->info->nb_eat)
-			{
-				//printf("%ld %d -----has finish\n", get_time(phil->info), i);
-				phil->finish = 1;
-				usleep(20);	
-			}
-			take_drop_fork(0, phil);
-		}
-		//if(!check_turn_off(phil->info))
-		//{
-			print_state("is sleeping\n", phil, phil->info);
-			ft_usleep(phil->info->time_sleep /** 1000*/, phil->info);
-			//if (ft_usleep(phil->info->time_sleep /** 1000*/, phil->info))
-			//	printf("%ld %dAWAKE from sleep\n", get_time(phil->info), i);//DEBUG
+			eat(phil);		
+		print_state("is sleeping\n", phil, phil->info);
+		ft_usleep(phil->info->time_sleep, phil->info);
+		//if (ft_usleep(phil->info->time_sleep /** 1000*/, phil->info))
+		//	printf("%ld %dAWAKE from sleep\n", get_time(phil->info), i);//DEBUG
 		//}
 	}
 	//printf("LEAVING routine %d (%d)\n", i, phil->count_eat);
 	return (0);
 }
 
-int check_data(char *argv, int index)
-{
-	int i;
 
-	i = 0;
-	while(argv[i])
-	{
-		if (!ft_isdigit(argv[i]))
-			return (0);
-		i++;
-		if (i > 10)
-		{
-			printf("enter an integer please\n");
-			return (0);
-		}
-	}
-	return (1);
-}
-
+//30 LIGNES TODO 
 int		init(t_info *info, t_phil **phil)
 {
 	//printf("INIT\n");
@@ -256,13 +244,21 @@ int		init(t_info *info, t_phil **phil)
 	i = 0;
 	*phil = malloc(sizeof(t_phil) * info->nb_philo);
 	info->fork = malloc(sizeof(pthread_mutex_t) * info->nb_philo);
+	if (!*phil || !info->fork)
+		return (1);
 	info->turn_off = 0;
 	while (i < info->nb_philo)
 	{
-		pthread_mutex_init(&info->fork[i], NULL);
+		if(pthread_mutex_init(&info->fork[i], NULL))
+			return (1);
+		if(pthread_mutex_init(&(*phil)[i].check_meal, NULL))
+			return (1);
 		i++;
 	}
-
+	if(pthread_mutex_init(&info->print_turn, NULL))
+		return (1);
+	if(pthread_mutex_init(&info->check_off, NULL))
+		return (1);
 	i = 0;
 	while (i < info->nb_philo)
 	{
@@ -276,28 +272,61 @@ int		init(t_info *info, t_phil **phil)
 	return(0);
 }
 
-int		destroy(t_info *info, t_phil **phil)
+void	free_philo(t_phil **phil, t_info *info)
 {
-	//printf("DESTROY LA FAMILLE \n");
+		ft_free((void**)&info->fork);
+		ft_free((void**)phil);
+}
+
+int		exit_philo(t_phil **phil, t_info *info, int exit_val)
+{
 	int i;
 	
 	i = 0;
 	while (i < info->nb_philo)
 	{
-	//	printf("JOIN %d\n", i);
+		//printf("JOIN %d\n", i);
 		pthread_join((*phil)[i].philo, NULL);
 		i++;
 	}
-
 	i = 0;
 	while (i < info->nb_philo)
 	{
 	//	printf("MUTEX DESTROY %d\n", i);
 		pthread_mutex_destroy(&info->fork[i]);
+		pthread_mutex_destroy(&(*phil)[i].check_meal);
 		i++;
 	}
-	//printf("LEAVE DESTROY\n");
-	return (0);
+	pthread_mutex_destroy(&info->check_off);
+	free_philo(phil, info);
+	return (exit_val);
+}
+
+int check_data(char *argv, int index)
+{
+	int i;
+
+	i = 0;
+	while(argv[i])
+	{
+		if (!ft_isdigit(argv[i]))
+		{	
+			printf("enter a positive number please\n");
+			return (0);
+		}
+		if (i > 10)
+		{
+			printf("enter an integer please\n");
+			return (0);
+		}
+		if (index ==  1  && i > 2)
+		{
+			printf("be gentle : no more than 200 philosopher can have dinner at the same time\n");
+			return (0);
+		}
+		i++;
+	}
+	return (1);
 }
 
 int		parse(t_info *info, char **argv)
@@ -309,8 +338,8 @@ int		parse(t_info *info, char **argv)
 	{
 		if (!check_data(argv[i], i))
 		{
-			printf("parsing error\n");
-			exit_philo(NULL, NULL, 1);
+			//printf("parsing error\n");
+			return (1);
 		}
 		i++;
 	}
@@ -334,27 +363,29 @@ int		checker(t_info *info, t_phil **phil)
 	finish = 1;
 	while (i < info->nb_philo)
 	{
+		pthread_mutex_lock(&(*phil)[i].check_meal);
 		if((*phil)[i].finish == 0)
 			finish = 0;
-		
+		pthread_mutex_unlock(&(*phil)[i].check_meal);	
 	//	printf("%d (%ld - %ld)= %ld\n", i,get_time(info), (*phil)[i].last_eat, get_time(info) - (*phil)[i].last_eat);
+		pthread_mutex_lock(&(*phil)[i].check_meal);
 		if(get_time(info) - (*phil)[i].last_eat >= info->time_die)
 		{
 	//	printf("%d (%ld - %ld)= %ld\n", i,get_time(info), (*phil)[i].last_eat, get_time(info) - (*phil)[i].last_eat);
-			printf("======%ldms %d DIED (last_eat -> %ld)\n", get_time(info), i, (*phil)[i].last_eat);
-			exit_philo(info, phil, 0);
+		//	printf("======%ldms %d DIED (last_eat -> %ld)\n", get_time(info), i, (*phil)[i].last_eat);
+			print_state("died\n", *phil, info);
+			pthread_mutex_unlock(&(*phil)[i].check_meal);
+			return (1); 
 		}
+		pthread_mutex_unlock(&(*phil)[i].check_meal);
 		i++;
 	}
 	if (finish)
-	{
-		//printf("%ld FINISH OPERATION\n", get_time(info));
-		exit_philo(info, phil, 1);
-	}
+		return (1);
 	return(0);
 }
 
-int		philo(t_info *info, t_phil **phil)
+void	philo(t_info *info, t_phil **phil)
 {
 	int i;
 
@@ -370,9 +401,14 @@ int		philo(t_info *info, t_phil **phil)
 	while (1)
 	{
 		usleep(500);
-		checker(info, phil);
+		if (checker(info, phil))
+		{
+			pthread_mutex_lock(&info->check_off);
+			info->turn_off = 1;
+			pthread_mutex_unlock(&info->check_off);
+			break ;
+		}
 	}
-	return (0);
 }
 
 int 	main(int argc, char **argv)
@@ -383,13 +419,19 @@ int 	main(int argc, char **argv)
 	
 	if (argc == 6 || argc == 5)
 	{
-		parse(&info, argv);
-		init(&info, &phil);
-		philo(&info, &phil);
-		destroy(&info, &phil);
+		if (parse(&info, argv))
+			return (1);
+		if (!init(&info, &phil))
+		{
+			philo(&info, &phil);
+			return (exit_philo(&phil, &info, 0));
+		}	
 	}
 	else
+	{
 		printf("wrong number of arguments\n");
+		return (1);
+	}
 	return(0);
 }
 
